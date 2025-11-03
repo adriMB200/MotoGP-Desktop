eb6bcab8ac3cba250a1c5420e94117537a6116d5
Adrián Martís Blanco <36863094+adriMB200@users.noreply.github.com>
2025-11-03 12:17:12 +0100

    Tareas 1,2,3 Sesion 6 js

---
+++ js/ciudad.js
@@ -0,0 +1,13 @@
+"use strict";
+//Ciudad usada Doha en Catar 
+class Ciudad {
+    constructor(nombre, pais, gentilicio, poblacion, coordenadas) {
+        this.nombre = nombre;
+        this.pais = pais;
+        this.gentilicio = gentilicio;
+        this.poblacion = poblacion;
+        this.coordenadas = coordenadas; // 25.28678563337303, 51.53384774116898
+    }
+
+
+}
